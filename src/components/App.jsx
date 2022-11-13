2a8841c8db0b2b102347c7c728e230316b6c0c0a
DiChag <chagindmitry1@gmail.com>
2022-11-13 20:29:14 +0100

    changed-to-hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,127 +1,89 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
-// import { Modal } from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Катька Витькина', number: '459-12-56' },
-      { id: 'id-2', name: 'Колекторы не отвечать', number: '443-89-12' },
-      { id: 'id-3', name: 'Гусь с посёлка', number: '645-17-79' },
-      { id: 'id-4', name: 'Лёха Кабан', number: '227-91-26' },
-    ],
-    filter: '',
-    name: '',
-    number: '',
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Катька Витькина', number: '459-12-56' },
+    { id: 'id-2', name: 'Толя Жырный', number: '443-89-12' },
+    { id: 'id-3', name: 'Гусь с посёлка', number: '645-17-79' },
+    { id: 'id-4', name: 'Лёха Кабан', number: '227-91-26' },
+  ]);
 
-    // showModal: false,
+  const [firstRenderFlag, setFlag] = useState(true);
+
+  const [filter, setFilter] = useState('');
+  useEffect(() => {}, []);
+
+  useEffect(() => {
+    if (firstRenderFlag) {
+      const contactsFromLocalStorage = localStorage.getItem('contactList');
+
+      if (contactsFromLocalStorage !== 'undefined') {
+        const parsedContacts = JSON.parse(contactsFromLocalStorage);
+
+        if (parsedContacts) {
+          setContacts(parsedContacts);
+        }
+      }
+      setFlag(false);
+    } else {
+      localStorage.setItem('contactList', JSON.stringify(contacts));
+    }
+  }, [contacts, firstRenderFlag]);
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setFilter(value);
   };
 
-  componentDidMount() {
-    const contactsFromLocalStorage = localStorage.getItem('contactList');
-    const parsedContacts = JSON.parse(contactsFromLocalStorage);
-
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevStateContacts = prevState.contacts;
-    const nextStayContacts = this.state.contacts;
-
-    if (prevStateContacts !== nextStayContacts) {
-      localStorage.setItem('contactList', JSON.stringify(nextStayContacts));
-    }
-  }
-
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     const id = nanoid();
     const name = e.name;
     const number = e.number;
-    const contactsLists = [...this.state.contacts];
+    const contactsLists = [...contacts];
 
     if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
       alert(`${name} is already in contacts.`);
     } else {
-      contactsLists.push({ name, id, number });
+      contactsLists.push({ id, name, number });
     }
 
-    this.setState({ contacts: contactsLists });
+    setContacts(contactsLists);
   };
 
-  handleDelete = e => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== e),
-    }));
+  const handleDelete = e => {
+    setContacts(contacts.filter(contact => contact.id !== e));
   };
 
-  getFilteredContacts = () => {
-    const filterContactsList = this.state.contacts.filter(contact => {
-      return contact.name
-        .toLowerCase()
-        .includes(this.state.filter.toLowerCase());
+  const getFilteredContacts = () => {
+    const filterContactsList = contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
     });
-
     return filterContactsList;
   };
 
-  //  MODAL
-
-  // toggleModal = () => {
-  //   this.setState(({ showModal }) => ({
-  //     showModal: !showModal,
-  //   }));
-  // };
-
-  render() {
-    const { filter } = this.state;
-
-    return (
-      <div
-        style={{
-          // height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          color: '#010101',
-        }}
-      >
-        {/* <button type="button" onClick={this.toggleModal}>
-          Add contact
-        </button>
-        {showModal && (
-          <Modal>
-            <h1>Hello world</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-              provident molestias facilis dolore culpa tenetur aperiam eum,
-              accusantium voluptatibus expedita, nobis praesentium animi,
-              voluptates pariatur repellat odio. Error, facere ipsum?
-            </p>
-            <button type="button" onClick={this.toggleModal}>
-              X
-            </button>
-          </Modal>
-        )} */}
-        <h1>Phonebook</h1>
-        <ContactForm handleSubmit={this.handleSubmit} />
-        <h2> Contacts</h2>
-        <Filter filter={filter} handleChange={this.handleChange} />
-        <ContactList
-          contacts={this.getFilteredContacts()}
-          handleDelete={this.handleDelete}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        // height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <h2> Contacts</h2>
+      <Filter filter={filter} handleChange={handleChange} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
+};
